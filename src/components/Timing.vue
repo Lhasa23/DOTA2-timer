e9647cd77e51f22638bb29013baca802341a9a32
eleven <sydryy@gmail.com>
2022-11-23 02:27:09 -0800

    amend

--- src/components/Timing.vue
+++ src/components/Timing.vue
@@ -6,20 +6,13 @@
 
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue'
-import Speech from 'speak-tts'
 
 const props = defineProps<{
   now: string
 }>()
 
 onMounted(() => {
-  try {
-    speechInit()
-    speak()
-  } catch (e) {
-    console.log('uncatchable error')
-    console.log(e)
-  }
+  speak()
 })
 
 const timing = ref(props.now)
@@ -30,10 +23,6 @@ const second = computed(() => timing.value.split(':')[1])
 const speech = ref(window.speechSynthesis)
 const chineseVoice = computed(() => speech.value.getVoices().find((value) => value.lang === 'zh-CN'))
 
-const speechInit = () => {
-  // speech.value.setLanguage('zh-CN')
-  // speech.value.init()
-}
 const speak = () => {
   let u = new window.SpeechSynthesisUtterance()
   u.voice = chineseVoice.value
